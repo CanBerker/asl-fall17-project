c45cc4f59aef1c25adaf0094d9bb98805b7e386a
canberker <canberkerwork@gmail.com>
2017-10-14 20:36:38 +0200

    Implement RequestQueue

--- src/ch/ethz/asltest/MyMiddleware.java
+++ src/ch/ethz/asltest/MyMiddleware.java
@@ -8,6 +8,9 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 
 public class MyMiddleware {
@@ -20,6 +23,8 @@ public class MyMiddleware {
     private int numThreadsPTP;
     private boolean readSharded;
 
+    protected static BlockingQueue<String> requestsQueue;
+
 
     public MyMiddleware() {
     }
@@ -31,6 +36,8 @@ public class MyMiddleware {
         this.mcAddresses = mcAddresses;
         this.numThreadsPTP = numThreadsPTP;
         this.readSharded = readSharded;
+
+        this.requestsQueue = new LinkedBlockingQueue<>();
     }
 
     public void run() {
@@ -46,7 +53,6 @@ public class MyMiddleware {
 
         }
     }
-
 
 
     private static class NetworkThread extends Thread {
@@ -72,7 +78,7 @@ public class MyMiddleware {
                     newClientHandler.start();
                 }
             } catch (IOException e) {
-                System.out.println("Oh crap.");
+                System.out.println("LOG: NetworkThread: Server socket failed to accept connection.");
                 e.printStackTrace();
             } finally {
                 closeSocket();
@@ -86,7 +92,6 @@ public class MyMiddleware {
                 e.printStackTrace();
             }
         }
-
 
         public List<ClientHandler> getClientHandlerList() {
             return clientHandlerList;
@@ -113,36 +118,83 @@ public class MyMiddleware {
 
         public void run() {
 
-            System.out.println("Oh, new blood!");
+            System.out.println("Client connected!");
             incrementUserCount();
 
             try {
-                out = new PrintWriter(clientSocket.getOutputStream(), true);
                 in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 String inputLine;
                 while ((inputLine = in.readLine()) != null) {
                     // Log the client text
                     System.out.println("A client said the following:" + "\t" + inputLine);
-                    out.println(String.format("Server[%d]: \"%s\"", activeUserCount, inputLine));
 
-                    if (".".equals(inputLine)) {
-                        System.out.println("Bye, friend.");
-                        decrementUserCount();
-                        clientSocket.close(); // may be redundant
-                        break;
-                    }
+                    requestsQueue.add(inputLine);
                 }
 
                 System.out.println("Closing socket things then");
                 in.close();
-                out.close();
                 clientSocket.close();
 
             } catch (IOException e) {
+                System.out.println("LOG: ClientHandlder: Reading from socket stream failed.");
+                e.printStackTrace();
+            } catch (IllegalStateException e) {
+                System.out.println("LOG: ClientHandler: Insertion to request queue failed.");
                 e.printStackTrace();
             }
         }
     }
+
+//    private static class ClientHandler extends Thread {
+//        private Socket clientSocket;
+//        private PrintWriter out;
+//        private BufferedReader in;
+//        private static int activeUserCount = 0;
+//
+//        public ClientHandler(Socket socket) {
+//            this.clientSocket = socket;
+//        }
+//
+//        public void incrementUserCount() {
+//            activeUserCount += 1;
+//        }
+//
+//        public void decrementUserCount() {
+//            activeUserCount -= 1;
+//        }
+//
+//        public void run() {
+//
+//            System.out.println("Client connected!");
+//            incrementUserCount();
+//
+//            try {
+//                out = new PrintWriter(clientSocket.getOutputStream(), true);
+//                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//                String inputLine;
+//                while ((inputLine = in.readLine()) != null) {
+//                    // Log the client text
+//                    System.out.println("A client said the following:" + "\t" + inputLine);
+//                    out.println(String.format("Server[%d]: \"%s\"", activeUserCount, inputLine));
+//
+//                    if (".".equals(inputLine)) {
+//                        System.out.println("Bye, friend.");
+//                        decrementUserCount();
+//                        clientSocket.close(); // may be redundant
+//                        break;
+//                    }
+//                }
+//
+//                System.out.println("Closing socket things then");
+//                in.close();
+//                out.close();
+//                clientSocket.close();
+//
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 
 
 
