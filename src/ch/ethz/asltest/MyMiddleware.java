4fa058158e2fc9c6d264cdf41097fd3cb51ad686
canberker <canberkerwork@gmail.com>
2017-10-14 22:35:04 +0200

    Implement Worker Thread Dequeue Operation

--- src/ch/ethz/asltest/MyMiddleware.java
+++ src/ch/ethz/asltest/MyMiddleware.java
@@ -7,6 +7,7 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
@@ -15,15 +16,16 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class MyMiddleware {
 
-    private ServerSocket networkSocket;
+    // Variables are defined as protected to be used in subclasses
+    protected String myIp;
+    protected int networkPort;
+    protected static List<String> mcAddresses;
+    protected int numThreadsPTP;
+    protected boolean readSharded;
 
-    private String myIp;
-    private int networkPort;
-    private List<String> mcAddresses;
-    private int numThreadsPTP;
-    private boolean readSharded;
-
+    protected static NetworkThread netThread;
     protected static BlockingQueue<String> requestsQueue;
+    protected List<WorkerThread> workerThreads;
 
 
     public MyMiddleware() {
@@ -37,14 +39,15 @@ public class MyMiddleware {
         this.numThreadsPTP = numThreadsPTP;
         this.readSharded = readSharded;
 
+        this.netThread = new NetworkThread(networkPort);
         this.requestsQueue = new LinkedBlockingQueue<>();
+        this.workerThreads = new ArrayList<>();
     }
 
     public void run() {
         try {
-            new NetworkThread(networkPort).start();
-            // startWorkerThreads(numThreadsPTP);
-
+            netThread.start();
+            startWorkerThreads();
 
         } catch (Exception e) {
 
@@ -53,7 +56,6 @@ public class MyMiddleware {
 
         }
     }
-
 
     private static class NetworkThread extends Thread {
 
@@ -81,14 +83,15 @@ public class MyMiddleware {
                 System.out.println("LOG: NetworkThread: Server socket failed to accept connection.");
                 e.printStackTrace();
             } finally {
-                closeSocket();
+                closeThread();
             }
         }
 
-        public void closeSocket() {
+        private void closeThread () {
             try {
                 networkSocket.close();
             } catch (IOException e) {
+                System.out.println("LOG: NetworkThread: Error while closing sockets.");
                 e.printStackTrace();
             }
         }
@@ -125,15 +128,9 @@ public class MyMiddleware {
                 in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 String inputLine;
                 while ((inputLine = in.readLine()) != null) {
-                    // Log the client text
-                    System.out.println("A client said the following:" + "\t" + inputLine);
-
                     requestsQueue.add(inputLine);
                 }
 
-                System.out.println("Closing socket things then");
-                in.close();
-                clientSocket.close();
 
             } catch (IOException e) {
                 System.out.println("LOG: ClientHandlder: Reading from socket stream failed.");
@@ -141,9 +138,116 @@ public class MyMiddleware {
             } catch (IllegalStateException e) {
                 System.out.println("LOG: ClientHandler: Insertion to request queue failed.");
                 e.printStackTrace();
+            } finally {
+                closeThread();
+            }
+        }
+
+        private void closeThread () {
+            try
+            {
+                in.close();
+                clientSocket.close();
+            } catch (IOException e) {
+                System.out.println("LOG: ClientHandler: Error while closing readers or sockets.");
+                e.printStackTrace();
             }
         }
     }
+
+
+    private void startWorkerThreads() {
+        for(int threadCount = 0; threadCount < numThreadsPTP; threadCount++) {
+            WorkerThread wt = new WorkerThread();
+            wt.start();
+        }
+    }
+
+    private static class WorkerThread extends Thread {
+        private List<Socket> serverSockets;             // keep server sockets, writers and readers in seperate lists
+        private List<PrintWriter> serverWriters;
+        private List<BufferedReader> serverReaders;
+
+
+        public WorkerThread() {
+            try
+            {
+                this.serverSockets = new ArrayList<>();
+                this.serverWriters = new ArrayList<>();
+                this.serverReaders = new ArrayList<>();
+                for (String address : mcAddresses) {
+                    String[] addressParts = address.split(":");     // split the IP and port values at ":"
+                    String ip = addressParts[0];
+                    int port = Integer.parseInt(addressParts[1]);       // cast port to integer
+
+                    System.out.println(ip + " " + port);
+
+                    Socket serverSocket = new Socket(ip,port);
+                    this.serverSockets.add(serverSocket);
+
+                    this.serverWriters.add(new PrintWriter(serverSocket.getOutputStream(), true));
+                    this.serverReaders.add(new BufferedReader(new InputStreamReader(serverSocket.getInputStream())));
+                }
+            }
+            catch (IOException e) {
+                System.out.println("LOG: Worker Thread: Error while creating sockets to servers.");
+                e.printStackTrace();
+            }
+        }
+
+        public void run() {
+            try {
+
+                String request;
+                int roundRobinIndex = 0;
+                while((request = requestsQueue.take()) != null)
+                {
+                    // TODO: forward the request to server
+                    // TODO: read response from server
+                    // TODO: forward the response to the client
+
+
+                    // serverWriters.get(roundRobinIndex).println();
+
+                    String inputLine;
+                    while ((inputLine = serverReaders.get(roundRobinIndex).readLine()) != null) {
+                        // Log the client text
+                        System.out.println("A client said the following:" + "\t" + inputLine);
+
+                    }
+
+
+                    roundRobinIndex = (roundRobinIndex + 1) % mcAddresses.size();      // increment the round robin index and take modulo with number of servers
+                }
+
+
+
+            } catch (IOException e) {
+                System.out.println("LOG: Worker Thread: Error occurred during read/write operation with server socket.");
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                System.out.println("LOG: Worker Thread: Error while reading from the requests queue.");
+                e.printStackTrace();
+            } finally {
+                closeThread();
+            }
+        }
+
+        private void closeThread () {
+            try
+            {
+                for (int arrayIndex = 0; arrayIndex < serverSockets.size(); arrayIndex++) {
+                    serverReaders.get(arrayIndex).close();
+                    serverWriters.get(arrayIndex).close();
+                    serverSockets.get(arrayIndex).close();
+                }
+            } catch (IOException e) {
+                System.out.println("LOG: WorkerThread: Error while closing readers, writers or sockets.");
+                e.printStackTrace();
+            }
+        }
+    }
+
 
 //    private static class ClientHandler extends Thread {
 //        private Socket clientSocket;
