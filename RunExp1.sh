58e206a81e2fc3dc8b5ab0b79aac2677edde6427
canberker <canberkerwork@gmail.com>
2017-11-06 19:28:54 +0100

    improve script

--- RunExp1.sh
+++ RunExp1.sh
@@ -53,18 +53,17 @@ suffix=".westeurope.cloudapp.azure.com"
 
 
 # experiment configurations
-ratio=${writeonly}
-
-threadCount=2
-clientCount=1
-testTime=2
-pingInterval=1
-
-
-
 IDservers=(6) #(6 7 8)
 IDmws=(4 5)
 IDclients=(1 2 3) #(1 2 3)
+
+testTime=2
+pingInterval=1
+
+ratio=${writeonly}
+threadCount=2
+virtualClientCount=1
+
 
 
 # part to control the server
@@ -73,13 +72,15 @@ for IDs in "${IDservers[@]}"; do
 (
 	ssh ${prefix}${IDs}${suffix} << EOSSH 
 	mkdir -p ${dirname}
-	rm -f ${dirname}/*
+	#rm -f ${dirname}/*   # clear previous logs if necessary
+	
 	screen -d -m -S memcached bash -c 'memcached -A -v -p 11211 > ${dirname}/${now}_server_${IDs}.txt'
 	EOSSH
 ) &
 done
 wait
 
+# implement VC Client Count loop
 
 
 now=$(date +"%Y%m%d_%H%M%S")
@@ -87,11 +88,13 @@ for IDc in "${IDclients[@]}"; do
 (
 	ssh ${prefix}${IDc}${suffix} << EOSSH 
 	mkdir -p ${dirname}
-	rm -f ${dirname}/*
 
 	# start pinging -- takes 2 secs longer than test time
 	screen -d -m -S pinger bash -c 'timeout $(($testTime+2)) ping -i ${pingInterval} ${prefix}${IDserver}${suffix} > ${dirname}/${now}_ping_${IDc}_${IDservers[0]}.log'
-	screen -d -m -S memtier bash -c '/home/can/memtier_benchmark-master/memtier_benchmark --server=${prefix}${IDservers[0]}${suffix} --port=11211 --protocol=memcache_text --threads=${threadCount} --clients=${clientCount} --test-time=${testTime} --ratio=${ratio} --expiry-range=9999-10000 --key-maximum=1000 --hide-histogram --out-file=${dirname}/${now}_report_${IDc}_${IDservers[0]}.txt'
+	screen -d -m -S memtier bash -c '/home/can/memtier_benchmark-master/memtier_benchmark --server=${prefix}${IDservers[0]}${suffix} --port=11211 --protocol=memcache_text --threads=${threadCount} --clients=${virtualClientCount} --test-time=${testTime} --ratio=${ratio} --expiry-range=9999-10000 --key-maximum=1000 --hide-histogram --out-file=${dirname}/${now}_report_${IDc}_${IDservers[0]}.txt'
+	
+	# add more ping report screens per active server
+
 	EOSSH
 ) &
 done
@@ -102,16 +105,30 @@ wait
 
 
 
+# GET CLIENT LOGS
+for IDc in "${IDclients[@]}"; do
+	rsync -avzhe ssh can@${prefix}${IDc}${suffix}:/home/can/experiment1 /home/can/asl-experiments		# –remove-source-files
+done
+wait
+
+# GET SERVER LOGS
+for IDs in "${IDservers[@]}"; do
+	rsync -avzhe ssh can@${prefix}${IDs}${suffix}:/home/can/experiment1 /home/can/asl-experiments
+done
+wait
 
 
 
+# REMOVE LOGS AT CLIENT
+for IDc in "${IDclients[@]}"; do
+(
+	ssh ${prefix}${IDc}${suffix} << EOSSH 
+	rm -f ${dirname}/*
 
-rsync -avzhe ssh can@${prefix}${IDclient1}${suffix}:/home/can/experiment1 /home/can/asl-experiments
-rsync -avzhe ssh can@${prefix}${IDclient2}${suffix}:/home/can/experiment1 /home/can/asl-experiments
-rsync -avzhe ssh can@${prefix}${IDclient3}${suffix}:/home/can/experiment1 /home/can/asl-experiments
-
-
-
+	EOSSH
+) &
+done
+wait
 
 
 
